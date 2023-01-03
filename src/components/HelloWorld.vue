e36d89c5be6c734d536006c2956a986cb80dc50e
Yigit Yurtseven <y.yurtseven@outlook.com>
2023-01-03 23:41:29 +0300

    pagination added. have known issues

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,35 @@
 <template>
-  <div class="main-container bg-slate-900 ">
-    <div class="flex flex-wrap flex-row justify-center" v-if="data">
+  <div class="main-container bg-[#222222] ">
+    <div class="loader" v-if="isLoading">
+      <img src="../assets/loader.gif" alt="">
+    </div>
+    <div class="flex flex-wrap flex-row justify-center" v-if="data && !isLoading">
+      <div class="w-full flex items-center justify-center">
+        <img src="../assets/logo1.png" style="width: 250px;" alt="">
+        <p class="self-end mb-5 text-green-500 font-bold">wiki</p>
+      </div>
+      <div class="w-full flex justify-center items-center border-red-500">
+        <label class="relative block">
+          <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+            <svg class="h-5 w-5 fill-slate-700" width="24px" height="24" viewBox="0 0 48 48"
+              xmlns="http://www.w3.org/2000/svg">
+
+              <path d="M0 0h48v48H0z" fill="none" />
+              <g id="Shopicon">
+                <path d="M18,32c3.144,0,6.036-1.049,8.373-2.799L40,42.829L42.829,40L29.201,26.373C30.951,24.036,32,21.144,32,18
+		c0-7.732-6.268-14-14-14S4,10.268,4,18S10.268,32,18,32z M18,8c5.514,0,10,4.486,10,10c0,5.514-4.486,10-10,10
+		c-5.514,0-10-4.486-10-10C8,12.486,12.486,8,18,8z" />
+              </g>
+            </svg>
+          </span>
+          <input v-model="searchValue"
+            class="placeholder:italic placeholder:text-slate-600 block w-64 bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-green-500 focus:ring-green-500 focus:ring-1 sm:text-sm"
+            placeholder="Search..." type="text" name="search" />
+        </label>
+        <button type="button"
+          class="rounded bg-green-500 ml-3 px-3 py-[6px] hover:bg-green-600 ease-in-out duration-200 cursor-pointer"
+          @click="searchClicked(searchValue)">Search</button>
+      </div>
       <div v-for="char in characters" :key="char.id"
         class="mx-5 my-5 bg-slate-700 border-2 rounded border-solid group border-green-500 drop-shadow-lg ease-in-out duration-200 hover:drop-shadow-[0_35px_35px_rgba(6,214,160,0.55)] cursor-pointer"
         @click='characterDetail(char)'>
@@ -15,6 +44,10 @@
           </div>
         </div>
       </div>
+    </div>
+    <div v-if="!isLoading" class="flex justify-center items-center mt-2 pb-2">
+      <vue-awesome-paginate :total-items="data.info.count" :items-per-page="20" :max-pages-shown="5"
+        v-model="currentPage" :on-click="pageChanged" />
     </div>
   </div>
   <template>
@@ -89,12 +122,55 @@ export default {
       characters: null,
       isCharacterModalVisible: false,
       characterInformation: null,
+      isLoading: true,
+      currentPagee: 1,
+      currentPage: 1,
+      searchValue: "",
+    }
+  },
+  mounted() {
+    document.onreadystatechange = () => {
+      if (document.readyState == "complete") {
+        setTimeout(() => {
+          this.isLoading = false;
+        }, 2500);
+
+      }
     }
   },
   created() {
     this.getData()
   },
   methods: {
+    pageChanged(page) {
+      // TODO : PAGİNATION ONLY WORKS FOR 1 FORWARD AND 1 BACKWARD. LOOK AT THE DIFFERENCE AND MAKE A REQUEST ACCORDING TO IT
+      if (this.currentPagee < page) {
+        axios.get(this.data.info.next).then(x => {
+          console.log(x)
+          this.currentPagee = page
+          this.data = x.data;
+          this.characters = x.data.results
+          setTimeout(() => {
+            this.isLoading = false;
+          }, 1000);
+        })
+      }
+      else if (this.currentPagee > page) {
+        axios.get(this.data.info.prev).then(x => {
+          this.currentPagee = page
+          this.data = x.data;
+          this.characters = x.data.results
+          setTimeout(() => {
+            this.isLoading = false;
+          }, 1000);
+        })
+      }
+      else {
+        console.log("Page", page, "current page : ", this.currentPagee)
+        console.log("zortladın knk")
+      }
+
+    },
     getData() {
       axios.get('https://rickandmortyapi.com/api/character').then(x => {
         console.log(x)
@@ -105,11 +181,65 @@ export default {
     characterDetail(char) {
       this.characterInformation = char;
       this.isCharacterModalVisible = true;
+    },
+    searchClicked(value) {
+      let trimmedVal = encodeURIComponent(value.trim())
+      axios.get('https://rickandmortyapi.com/api/character/?name=' + trimmedVal).then(x => {
+        console.log(x)
+        this.data = x.data;
+        this.characters = x.data.results
+      })
     }
 
   }
 }
 </script>
 <style scoped>
+.loader {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #222222;
+}
+</style>
+<style>
+.pagination-container {
+  display: flex;
+  column-gap: 10px;
+}
 
+.paginate-buttons {
+  height: 40px;
+  width: 40px;
+  border-radius: 3px;
+  cursor: pointer;
+  background-color: rgb(31, 31, 31);
+  border: 1px solid rgb(48, 48, 48);
+  color: white;
+}
+
+.paginate-buttons:hover {
+  background-color: #71d68fd4;
+}
+
+.active-page {
+  background-color: #27e05fd4;
+  border: 1px solid #27e05fd4;
+  color: white;
+}
+
+.active-page:hover {
+  background-color: #2988c8;
+}
+
+@media screen and (max-width:480px) {
+  .pagination-container {
+    width: 90vw;
+    align-items: center;
+    justify-content: center;
+    column-gap: 2px;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import VueAwesomePaginate from "vue-awesome-paginate";
+import "vue-awesome-paginate/dist/style.css";
 import "./style.css";
-createApp(App).mount("#app");
+createApp(App).use(VueAwesomePaginate).mount("#app");
